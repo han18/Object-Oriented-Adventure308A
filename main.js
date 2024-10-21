0968252844188bad45d497dfb5d6ee1422fa9781
han18 <han.fadah@gmail.com>
2024-10-21 16:01:57 -0400

    extending the class

--- main.js
+++ main.js
@@ -7,14 +7,12 @@ const adventurer = {
   health: 10,
   inventory: ["sword", "potion", "artifact"],
   companion: {
-    name: {
-      Leo: {
-        name: "Frank",
-        type: "Flea",
-        belongings: ["hat", "sunglasses"],
-      },
-             },
+    name: "Leo",
     type: "Cat",
+    companion: {
+      name: "Frank",
+      type: "Flea",
+    },
   },
   roll(mod = 0) {
     const result = Math.floor(Math.random() * 20) + 1 + mod;
@@ -30,17 +28,33 @@ for (const x in adventurer) {
   console.log(adventurer[x]);
 }
 
-
-//calling the method roll to test 
-adventurer.roll()
+//calling the method roll to test
+adventurer.roll();
 
 // Part 2: Class Fantasy
 
+// this class is the template object
 class Character {
-    constructor (name) {
-      this.name = name;
-      this.health = 100;
-      this.inventory = [];
-    }
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
   }
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+  
+}
 
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+
+// Part 3: Class Features
+// extending a class, the “child” class inherits all properties of its parents
